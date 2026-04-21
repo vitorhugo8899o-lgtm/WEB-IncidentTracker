1f75e908553810673eb916ba011d7adc2efeefea
Vítor <vitorhugo8899o@gmail.com>
2026-04-21 17:04:28 -0300

    feat: rota das paginas

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
+
+const routers = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  }
+])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routers} />
   </StrictMode>,
 )
