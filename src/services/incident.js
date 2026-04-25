3e1cdb293ff31a7354b91cbc9dedb6f19bac0724
Vítor <vitorhugo8899o@gmail.com>
2026-04-24 21:20:34 -0300

    feat: busca personalizada de incidents por tecnicos

--- src/services/incident.js
+++ src/services/incident.js
@@ -47,7 +47,23 @@ async function GetIncidents() {
     }
 }
 
+async function QueryIncidents(offset, limit, status, priority, create_at, creator) {
+    const params = new URLSearchParams();
 
+    if (filtros.offset != null) params.append("offset", filtros.offset);
+    if (filtros.limit != null) params.append("limit", filtros.limit);
+    if (filtros.status != null) params.append("status", filtros.status);
+    if (filtros.priority != null) params.append("priority", filtros.priority);
+    if (filtros.created_at != null) params.append("created_at", filtros.created_at);
+    if (filtros.creator != null) params.append("creator", filtros.creator);
+
+    const response = await api(`/api/v1/incidents?${params.toString()}`, {
+        method: "GET",
+        credentials: 'include'
+    })
+
+    return response
+}
 
 
 
