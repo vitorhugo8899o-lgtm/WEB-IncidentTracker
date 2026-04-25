4ae9639bfa42ba6c2bac9c599e8049b3e1b599d7
Vítor <vitorhugo8899o@gmail.com>
2026-04-24 21:05:03 -0300

    feat: criacao da features de abrir chamados

--- src/services/incident.js
+++ src/services/incident.js
@@ -1,6 +1,25 @@
 import { api } from "./api";
 
 
+async function CreateIncident(data) {
+    const payload = {
+        title: data.title,
+        description: data.description,
+    }
+
+    try {
+        const response = await api("/api/v1/incidents", {
+            method: "POST",
+            credentials: 'include',
+            body: JSON.stringify(payload)
+        })
+
+        return response
+    } catch (error) {
+        throw new Error(error.message || "Erro de conexão com o servidor.");
+    }
+}
+
 async function GetIncidents() {
     try {
         const response = await api("/api/v1/user_incidents", {
@@ -20,4 +39,4 @@ async function GetIncidents() {
 
 
 
-export { GetIncidents };
+export { GetIncidents, CreateIncident };
